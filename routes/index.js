baaed91010ab39b494f8aa472cc20962e8961031
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-01-31 12:03:19 -0600

    Agregamos nueva ruta

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 // 1. IMPORTACIONES
 const express = require("express");
+const res = require("express/lib/response");
 const router = express.Router();
 
 // 2. RUTEO
 router.get("/", (req, res) => {
     res.send("Hola mundo");
+})
+
+router.get("/contacto", () => {
+    res.send("Contacto");
 })
 
 // 3. EXPORTACION
